96352b4fbb05d627d1d822f5806c22aa34b105dd
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2026-09-24 21:06:34 +0530

    Parent to Child Data Passing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Child } from './child/child';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Child],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-tut');
+
+  userName = signal("Viren Mali");
+
+  users = signal(["Brus", "Mike", 'Ronda']);
+
+  newUser = signal("");
+
+  AddNewUser() {
+    this.users.update(item => [...item, this.newUser()]);
+    this.newUser.set("");
+  }
 
 }
